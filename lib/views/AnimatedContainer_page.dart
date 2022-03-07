90ea86fc035d0f58c5c9628be63fc7898b885b60
Elite37 <elite37oficial@gmail.com>
2022-03-07 14:44:34 -0300

    #4 AnimatedContainer

---
+++ lib/views/AnimatedContainer_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerPage extends StatefulWidget {
+  const AnimatedContainerPage({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedContainerPage> createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            selected = !selected;
+          });
+        },
+        child: Center(
+          child: AnimatedContainer(
+            child: (Text(
+              selected.toString(),
+              textAlign: TextAlign.center,
+            )),
+            width: selected ? 400 : 200,
+            height: 200,
+            color: selected ? Colors.yellow : Colors.blue,
+            duration: Duration(seconds: 5),
+          ),
+        ),
+      ),
+    );
+  }
+}
